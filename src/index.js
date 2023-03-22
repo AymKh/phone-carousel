d39a608c3351fbf6c1ef57e3b4e963e55d88a5e0
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-22 22:58:10 +0100

    autoSlide action

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const mainSlideElement = document.querySelector('#main-slide');
+const imageToSlide = document.querySelector('.image-to-slide');
+const timeToNextImage = 1000;
+let currentImageIndex = 0;
+
+const imagesList = [
+    '../assets/carousel-photo-01.jpg',
+    '../assets/carousel-photo-02.jpg',
+    '../assets/carousel-photo-03.jpg',
+    '../assets/carousel-photo-04.jpg'
+];
+
+
+const autoSlide = (_) => {
+    imageToSlide.setAttribute('src', imagesList[currentImageIndex]);
+    if (currentImageIndex < imagesList.length - 1)
+        currentImageIndex++;
+    else
+        currentImageIndex = 0;
+}
+
+
+
+
+setInterval(() => {
+    autoSlide();
+}, timeToNextImage);
